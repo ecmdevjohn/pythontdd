fe0c1baa411f75f483ffd057f1cc6c16f0b85604
John Anderson <ecmdevjohn@gmail.com>
2019-06-02 19:11:05 +0100

    enforce model validation in list view (after some timegit status)

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -50,7 +50,7 @@ class ListViewTest(TestCase):
 		)
 		self.assertEqual(response.status_code, 200)
 		self.assertTemplateUsed(response, 'list.html')
-		expected.error = escape("You can't have an empty list item")
+		expected_error = escape("You can't have an empty list item")
 		self.assertContains(response, expected_error)
 
 class NewListTest(TestCase):
