2f2a3aa10d302157f2e49706fc54a2d5377934ad
John Anderson <ecmdevjohn@gmail.com>
2019-06-08 00:35:13 +0100

    use form in all views, back to working state

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -35,7 +35,7 @@ class NewVisitorTest(FunctionalTest):
 		# There is still a text box inviting her to add another item.
 		# She enters "Use peacock feathers to make a fly"
 		# (Edith is very methodical)
-		inputbox = self.browser.get_item_input_box()
+		inputbox = self.get_item_input_box()
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 
